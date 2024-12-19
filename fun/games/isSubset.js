7f7e18e23cfb978af78f0d1cde7bcafd69cbfd48
PruthviLahare <142437821+PruthviLahare@users.noreply.github.com>
2024-12-19 17:24:09 +0530

    Delete fun/games directory

--- fun/games/isSubset.js
+++
@@ -1,9 +0,0 @@
-const love_ = function (set, subset) {
-  const s = function (current) {
-    return set.includes(current);
-  };
-
-  return subset.every(s);
-};
-
-console.log(isSubset([...'3456'], [...'156']));
